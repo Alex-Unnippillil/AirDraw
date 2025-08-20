25368c22d6ddd943a020280ef5ab00cab512050e
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-19 21:40:54 -0400

    Fix conflicts: removed problematic lines from 7 files

--- packages/web/src/components/PrivacyToggle.tsx
+++ packages/web/src/components/PrivacyToggle.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import { usePrivacy } from '../context/PrivacyContext';
-
-export function PrivacyToggle() {
-  const { enabled, toggle } = usePrivacy();
-  return (
-    <button data-testid="privacy-toggle" onClick={toggle}>
-      {enabled ? 'Disable Privacy' : 'Enable Privacy'}
-    </button>
-  );
-}
-
-export default PrivacyToggle;
+﻿
