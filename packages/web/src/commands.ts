fa0af168da722d6403159a9ac2088c75073e7cc7
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 01:01:37 -0400

    Fix conflicts: removed problematic lines from 7 files

--- packages/web/src/commands.ts
+++ packages/web/src/commands.ts
@@ -1,9 +1,1 @@
-import { CommandOf } from '@airdraw/core';
-
-export interface AppCommands {
-  setColor: { hex: string };
-  undo: {};
-  redo: {};
-}
-
-export type AppCommand = CommandOf<AppCommands>;
+﻿
